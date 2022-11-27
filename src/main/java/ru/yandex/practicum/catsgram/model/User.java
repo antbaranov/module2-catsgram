7f7cf0b32e6044a2ed43a412e5dcd5e1205685d7
Anton <2639231@gmail.com>
2022-11-27 20:26:17 +0300

    psgsql

--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -4,16 +4,30 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class User {
-    private String email;
-    private String nickname;
-    private LocalDate birthdate;
 
-    public String getEmail() {
-        return email;
+    private String id;
+
+    private String username;
+
+    private String nickname;
+
+    public User() {
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getNickname() {
@@ -22,26 +36,5 @@ public class User {
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return email.equals(user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(email);
     }
 }
